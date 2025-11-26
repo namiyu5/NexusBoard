86c1d06a1201338c8a9adbbc5ca038fc747f1ed8
namiyu5 <iahmed0610@gmail.com>
2025-11-26 10:38:23 +0000

    Add Vue build to staticfiles

--- nexus_board/settings.py
+++ nexus_board/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'nexus_board.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'nexus_board' / 'staticfiles'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -138,7 +138,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'nexus_board' / 'staticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
