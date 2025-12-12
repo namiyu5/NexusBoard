fd7b0e83539fa75f3772b9ffb2e6d6125f732450
namiyu5 <iahmed0610@gmail.com>
2025-12-12 15:11:26 +0000

    Add new image assets and remove obsolete build script. Create Readme

--- build_vue.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd C:\Users\iahme\OneDrive\Belgeler\vscode-projects\NexusBoard\nexusboard_vue
-npm run build
-xcopy /E /I /Y dist C:\Users\iahme\OneDrive\Belgeler\vscode-projects\NexusBoard\nexusboard_django\static
-echo Vue build copied to Django static folder!
